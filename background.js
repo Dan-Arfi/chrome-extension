db462279157ebf39c7d089fceb2d7cdb659e9565
Dan-Arfi <danarfi07@gmail.com>
2023-07-06 19:34:41 +0300

    asdasd

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === "loading" && tab.url === "https://www.google.com/") {
-      chrome.tabs.update(tabId, { url: "chrome://newtab" });
+      chrome.tabs.update(tabId, { url: "https://app.eatself.com/admin-login" });
     }
   });
   
